6c3e504207b2c83f76e93c3842ed9240ab565e3a
I'm Luis! ^-^ <malbertoa_11@hotmail.com>
2016-01-10 19:46:04 -0400

    fix: cut option not working

--- assets/ajax/cut.php
+++ assets/ajax/cut.php
@@ -1,5 +1,6 @@
 <?php
 require $_SERVER['DOCUMENT_ROOT'] . '/load.php';
+require PATH . INC . 'class.audio.php';
 
 ('POST' !== getenv('REQUEST_METHOD') ) and exit();
 
@@ -14,36 +15,25 @@ if( ! array_key_exists($_POST['id'], $_SESSION) )
 
 if( is_numeric($_POST['start']) ) {
 	$start = (int) $_POST['start'];
-	if( $start < 0 || $start > 120 )
-		_result( __('Request malformed.'), false );
 }else{
 	$start = $_POST['start'];
 	if( ! preg_match('/^([0-9]{1,2}):([0-9]{1,2})$/', $start) )
 		_result( __('Request malformed.'), false );
 	$lel = explode(":", $start);
-	if( (int) $lel[0] > 2 || (int) $lel[1] > 60 )
-		_result( __('Request malformed.'), false );
-	if( (int) $lel[0] == 2 && (int) $lel[1] > 0 )
-		_result( __('Request malformed'), false );
 	$start = ( (int) $lel[0] * 60 ) + (int) $lel[1]; // in seconds
 }
 
-if( is_numeric($_POST['start']) ) {
-	$start = (int) $_POST['start'];
-	if( $start < 0 || $start > 120 )
-		_result( __('Request malformed.'), false );
+if( is_numeric($_POST['end']) ) {
+	$end = (int) $_POST['end'];
 }else{
 	$end = $_POST['end'];
 	if( ! preg_match('/^([0-9]{1,2}):([0-9]{1,2})$/', $end) )
 		_result( __('Request malformed.'), false );
 	$lel = explode(":", $end);
-	if( (int) $lel[0] > 2 || (int) $lel[1] > 60 )
-		_result( __('Request malformed.'), false );
-	if( (int) $lel[0] == 2 && (int) $lel[1] > 0 )
-		_result( __('Request malformed'), false );
 	$end = ( (int) $lel[0] * 60 ) + (int) $lel[1]; // in seconds
 }
-if( $start >= $end )
+$diff = $end-$start;
+if( ($start >= $end) || $diff > 120 || $diff < 0 )
 	result( __('Request malformed.') );
 
 $id = $_POST['id'];
