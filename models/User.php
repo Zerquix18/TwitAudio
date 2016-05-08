ec4ef5b0311eb593854ff8ed622ea96b730d363e
I'm Luis! ^-^ <malbertoa_11@hotmail.com>
2016-05-08 14:18:40 -0400

    fixed problem while logging

--- models/User.php
+++ models/User.php
@@ -200,10 +200,10 @@ class User extends \application\ModelBase {
 
 		$first_time = $this->db->query(
 				'SELECT COUNT(*) AS size FROM users
-				WHERE id = ?',
+				 WHERE id = ?',
 				$id
 			);
-		$first_time = !! $first_time->size;
+		$first_time = '0' === $first_time->size;
 
 		if( ! $first_time ) {
 			// re-update
@@ -218,10 +218,10 @@ class User extends \application\ModelBase {
 			) )->where('id', $id)->_();
 		}else{
 			// welcome, new user!
-			$favs_public =
+			$favs_public   =
 			$audios_public = (int) ! $details->protected;
-			$time = time();
-			$lang = $details->lang;
+			$time          = time();
+			$lang          = $details->lang;
 			$register_user = $this->db->insert("users", array(
 					'id'                  => $id,
 					'user'                => $user,
@@ -238,7 +238,7 @@ class User extends \application\ModelBase {
 				)
 			);
 			if( ! $register_user ) {
-				throw new \Exception('Database error: ' . $this->db->error);
+				throw new \Exception('Insert user error: ' . $this->db->error);
 			}
 		}
 
@@ -258,7 +258,7 @@ class User extends \application\ModelBase {
 			)
 		);
 		if( ! $register_session ) {
-			throw new \Exception('Database error: ' . $this->db->query);
+			throw new \Exception('Insert session error: ' . $this->db->query);
 		}
 
 		return array(
