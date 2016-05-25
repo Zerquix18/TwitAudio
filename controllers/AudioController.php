b42ef2a8e72cd71e24e62849ef3a1c7c6f12ccec
I'm Luis! ^-^ <malbertoa_11@hotmail.com>
2016-05-24 21:32:26 -0400

    Finish the audio page

--- controllers/AudioController.php
+++ controllers/AudioController.php
@@ -22,55 +22,68 @@ class AudioController {
 	 * @param string $audio_id
 	 */
 	public function __construct( $audio_id ) {
-		$audio = Audios::get($audio_id);
-		if( ! $audio ) {
-			View::exit_404();
-		}
-
-		$current_user = Users::get_current_user();
-		if( ! $current_user->can_listen( $audio['user']['user'] ) ) {
-			View::exit_404();
-		}
-
-		if( '0' !== $audio['reply_to'] ) {
-			HTTP::redirect(
-					url() . $audio['reply_to'] . '?reply_id=' . $audio['id']
-				);
-		}
-
-		$replies = Audios::get_replies($audio['id'], 1);
-		/** LINKED REPLIES **/
-		// Dragons be here
-		if( $reply_id = HTTP::get('reply_id') ) { // was the param sent?
-			$reply = Audios::get($reply_id);
-			if( ! empty($reply) && $reply['reply_to'] == $audio_id ) {
-				// reply exists and it's replying to this audio
-				$linked             = $reply_id;
-				$all_replies        = $replies;
-				$reply['is_linked'] = true; // to tell the display_audio function
-				$replies = array( // move everything
-						'audios'    => array(),
-						'load_more' => $all_replies['load_more'],
-						'page'      => $all_replies['page'],
-						'total'     => $all_replies['total']
-					);
-				$replies['audios'][] = $reply; // he goes first!
-				$count_replies       = count($all_replies['audios']);
-				for( $i = 0; $i < $count_replies; $i++ ) {
-					if( $all_replies['audios'][$i]['id'] == $reply_id ) {
-						/** don't add the linked reply, cuz it was added first **/
-						continue;
-					}
-					$replies['audios'][] = $all_replies['audios'][$i];
-				}
+		try {
+			$audio = Audios::get($audio_id);
+			if( ! $audio ) {
+				View::exit_404();
 			}
+
+			$current_user = Users::get_current_user();
+
+			if( ! $current_user->can_listen( $audio['user']['id'] ) ) {
+				View::exit_404();
+			}
+
+			if( '0' !== $audio['reply_to'] ) {
+				// if is trying to get into a reply
+				// redirect to the original audio
+				HTTP::redirect(
+						url() . $audio['reply_to'] .
+						'?reply_id=' . $audio['id']
+					);
+			}
+			$replies = Audios::get_replies($audio['id'], 1);
+			/** LINKED REPLIES **/
+			// Dragons be here
+			if( $reply_id = HTTP::get('reply_id') ) { // was the param sent?
+				$reply = Audios::get($reply_id);
+				if( ! empty($reply) && $reply['reply_to'] == $audio_id ) {
+					// reply exists and it's replying to this audio
+					$linked             = $reply_id;
+					$all_replies        = $replies;
+					$reply['is_linked'] = true; // to tell the display_audio function
+					$replies = array( // move everything
+							'audios'    => array(),
+							'load_more' => $all_replies['load_more'],
+							'page'      => $all_replies['page'],
+							'total'     => $all_replies['total']
+						);
+					$replies['audios'][] = $reply; // he goes first!
+					$count_replies       = count($all_replies['audios']);
+					for( $i = 0; $i < $count_replies; $i++ ) {
+						if( $all_replies['audios'][$i]['id'] == $reply_id ) {
+							/** don't add the linked reply, cuz it was added first **/
+							continue;
+						}
+						$replies['audios'][] = $all_replies['audios'][$i];
+					}
+				}
+			} /** / LINKED REPLIES **/
+
+			$twitter_share_url = '';
+
+			$bars = array(
+				'audio' => array(
+						'audio'		  => $audio,
+						'replies'	  => $replies,
+						'linked'      => isset($linked) ? $linked : '',
+					)
+				);
+			View::set_title('Audio');
+			View::set_page('audio');
+			echo View::get_group_template('main/audio', $bars);
+		} catch( \Exception $e ) {
+
 		}
-		/** / LINKED REPLIES **/
-		View::load_full_template('audio', array(
-				'audio'		=> $audio,
-				'replies'	=> $replies,
-				'linked'    => isset($linked) ? $linked : ''
-			)
-		);
 	}
 }
